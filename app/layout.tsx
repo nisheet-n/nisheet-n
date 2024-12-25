b5111ad60955cab40860a2e31c9fef1a13580ec4
nisheet-n <nisheet2000@gmail.com>
2024-12-25 23:18:23 +0530

    Lint fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Architects_Daughter, Comic_Neue, Indie_Flower, Rubik_Doodle_Shadow } from "next/font/google";
+import { Comic_Neue, Rubik_Doodle_Shadow } from "next/font/google";
 import "./globals.css";
 
 const rubicDoodle = Rubik_Doodle_Shadow({
